4521398e3839e9cbf28ac4c7a69d7cdb17050f02
Kaan Burak H. <salvetum@gmail.com>
2026-08-24 16:20:07 +0300

    fix: uretici gecmisi bos durum bildirimi padding'i saklanirken hayalet bosluk birakuyordu - Geçmiş başlığıyla ilk öge arasindaki ~46px bosluk kaldirildi (max-height:0 padding'i gizlemez); modals.css v69 + sw assets-v118

--- flask_app/templates/sw.js
+++ flask_app/templates/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kasa-v{{ APP_VERSION }}-assets-v117';
+const CACHE = 'kasa-v{{ APP_VERSION }}-assets-v118';
 const BG_URL_PREFIX = '/api/background/';
 const ASSETS = [
   '{{ url_for("static", filename="tokens.css") }}',
